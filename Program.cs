9b897c7fdc509053ee96cfd1fcf2b09b3c45ccd9
Stephen Gowen <sjgowen@gmail.com>
2023-09-16 16:38:29 +0100

    Tree program without using local functions

--- Program.cs
+++ Program.cs
@@ -1,34 +1,90 @@
-﻿namespace ModernCSharpFeatures;
+namespace ModernCSharpFeatures;
+
+public class Tree
+{
+    public int Value { get; }
+    private Tree Left { get; }
+    private Tree Right { get; }
+
+    public Tree(int value, Tree left = null, Tree right = null)
+    {
+        Value = value;
+        Left = left;
+        Right = right;
+    }
+
+    public void PrintInPreOrder(string format)
+    {
+        PrintInPreOrder(this, format);
+    }
+
+    public void PrintInOrder(string format)
+    {
+        PrintInOrder(this, format);
+    }
+
+    public void PrintInPostOrder(string format)
+    {
+        PrintInPostOrder(this, format);
+    }
+
+    private void PrintInPreOrder(Tree tree, string format)
+    {
+        if (tree == null) return;
+
+        PrintValue(tree, format);
+        PrintInPreOrder(tree.Left, format);
+        PrintInPreOrder(tree.Right, format);
+    }
+
+    private void PrintInOrder(Tree tree, string format)
+    {
+        if (tree == null) return;
+
+        PrintInOrder(tree.Left, format);
+        PrintValue(tree, format);
+        PrintInOrder(tree.Right, format);
+    }
+
+    private void PrintInPostOrder(Tree tree, string format)
+    {
+        if (tree == null) return;
+
+        PrintInPostOrder(tree.Left, format);
+        PrintInPostOrder(tree.Right, format);
+        PrintValue(tree, format);
+    }
+
+    private static void PrintValue(Tree tree, string format)
+    {
+        Console.Write($"{format} ", tree.Value);
+    }
+}
 
 public class Program
 {
-
-    public class Shape {}
-
-    public class Square : Shape
-    {
-        public int SideLength;
-    }
-
-    public class Circle : Shape
-    {
-        public int Radius;
-    }
-
-    public static double Area(Shape shape)
-    {
-        if (shape is Square square) return square.SideLength * square.SideLength;
-        if (shape is Circle circle) return circle.Radius * circle.Radius * Math.PI;
-
-        throw new InvalidOperationException("Invalid Shape");
-    }
-
     public static void Main(string[] args)
     {
-        var square = new Square { SideLength = 10 };
-        var circle = new Circle { Radius = 10 };
+        Console.WriteLine(@"Tree:
+         4
+       /   \
+      2     6
+     / \   / \
+    1   3 5   7
+        ");
 
-        Console.WriteLine($"Square area = {Area(square)}");
-        Console.WriteLine($"Circle area = {Area(circle)}");
+        var tree = new Tree(
+            4,
+            new Tree(2, new Tree(1), new Tree(3)),
+            new Tree(6, new Tree(5), new Tree(7))
+        );
+
+        Console.WriteLine("Print in pre order: ");
+        tree.PrintInPreOrder("{0}");
+        Console.WriteLine("\n\nPrint in order: ");
+        tree.PrintInOrder("[{0}]");
+        Console.WriteLine("\n\nPrint in post order: ");
+        tree.PrintInPostOrder("<{0}>");
+        Console.WriteLine();
     }
 }
